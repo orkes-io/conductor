a55b07127613490774d59adc07ff2683f101573a
Miguel Prieto <miguelprieto@orkes.io>
2022-06-10 16:40:38 -0400

    Quick and dirty fix for a NPE in orkes-conductor. This issue happens only when running orkes-conductor server on top of this conductor version.

--- core/src/main/java/com/netflix/conductor/core/execution/WorkflowExecutor.java
+++ core/src/main/java/com/netflix/conductor/core/execution/WorkflowExecutor.java
@@ -764,7 +764,8 @@ public class WorkflowExecutor {
             // update parent's sub workflow task
             TaskModel subWorkflowTask =
                     executionDAOFacade.getTaskModel(workflow.getParentWorkflowTaskId());
-            if (subWorkflowTask.getWorkflowTask().isOptional()) {
+            if (subWorkflowTask.getWorkflowTask() != null
+                    && subWorkflowTask.getWorkflowTask().isOptional()) {
                 // break out
                 LOGGER.info(
                         "Sub workflow task {} is optional, skip updating parents", subWorkflowTask);
