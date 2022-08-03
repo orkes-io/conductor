60e38333add86044eb0dccd5f62f4a95747389b8
Manan Bhatt <manan.bhatt@orkes.io>
2022-08-03 18:15:48 +0530

    Changing task mapper interface to make it extensible.
    GetMappedTask should return string instead of TaskType. That we we don't need to initialize the new taskMapper added explicitly.

--- core/src/test/java/com/netflix/conductor/core/execution/TestDeciderService.java
+++ core/src/test/java/com/netflix/conductor/core/execution/TestDeciderService.java
@@ -105,9 +105,9 @@ public class TestDeciderService {
         }
 
         @Bean
-        public Map<TaskType, TaskMapper> taskMapperMap(Collection<TaskMapper> taskMappers) {
+        public Map<String, TaskMapper> taskMapperMap(Collection<TaskMapper> taskMappers) {
             return taskMappers.stream()
-                    .collect(Collectors.toMap(TaskMapper::getTaskType, Function.identity()));
+                    .collect(Collectors.toMap(t -> t.getTaskType(), Function.identity()));
         }
 
         @Bean
@@ -132,7 +132,7 @@ public class TestDeciderService {
 
     @Autowired
     @Qualifier("taskMapperMap")
-    private Map<TaskType, TaskMapper> taskMappers;
+    private Map<String, TaskMapper> taskMappers;
 
     @Autowired private ParametersUtils parametersUtils;
 
