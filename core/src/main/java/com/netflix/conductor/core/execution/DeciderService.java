d0866666dcf700b7efe8d14f50c5683e3ecb008a
Manan Bhatt <manan.bhatt@orkes.io>
2022-08-04 09:21:54 +0530

    Review comments

--- core/src/main/java/com/netflix/conductor/core/execution/DeciderService.java
+++ core/src/main/java/com/netflix/conductor/core/execution/DeciderService.java
@@ -45,6 +45,7 @@ import com.netflix.conductor.model.WorkflowModel;
 
 import static com.netflix.conductor.common.metadata.tasks.TaskType.SIMPLE;
 import static com.netflix.conductor.common.metadata.tasks.TaskType.TERMINATE;
+import static com.netflix.conductor.common.metadata.tasks.TaskType.USER_DEFINED;
 import static com.netflix.conductor.model.TaskModel.Status.*;
 
 /**
@@ -861,7 +862,7 @@ public class DeciderService {
         // A new task must only be scheduled if a task, with the same reference name is not already
         // in this workflow instance
         return taskMappers
-                .getOrDefault(type, taskMappers.get(SIMPLE.name()))
+                .getOrDefault(type, taskMappers.get(USER_DEFINED.name()))
                 .getMappedTasks(taskMapperContext)
                 .stream()
                 .filter(task -> !tasksInWorkflow.contains(task.getReferenceTaskName()))
