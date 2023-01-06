0d5a38d015a4ccd32ddf1ad61680b9eec6f84628
manan164 <manan16489@gmail.com>
2023-01-07 02:02:23 +0530

    revert

--- core/src/main/java/com/netflix/conductor/core/execution/WorkflowExecutor.java
+++ core/src/main/java/com/netflix/conductor/core/execution/WorkflowExecutor.java
@@ -1176,7 +1176,9 @@ public class WorkflowExecutor {
         return dedupedTasks;
     }
 
-    /** @throws ConflictException if the workflow is in terminal state. */
+    /**
+     * @throws ConflictException if the workflow is in terminal state.
+     */
     public void pauseWorkflow(String workflowId) {
         try {
             executionLockService.acquireLock(workflowId, 60000);
