052c6da0c5552deb9e0a2f549cfe4f1853ff5554
manan164 <manan16489@gmail.com>
2023-01-07 02:18:30 +0530

    fixed tests

--- core/src/test/java/com/netflix/conductor/core/execution/TestWorkflowExecutor.java
+++ core/src/test/java/com/netflix/conductor/core/execution/TestWorkflowExecutor.java
@@ -314,9 +314,7 @@ public class TestWorkflowExecutor {
 
         boolean stateChanged = workflowExecutor.scheduleTask(workflow, tasks);
         assertEquals(2, startedTaskCount.get());
-        // Since task3 is at higher level then task 2 which is already scheduled so task3 will not
-        // get scheduled.
-        assertEquals(0, queuedTaskCount.get());
+        assertEquals(1, queuedTaskCount.get());
         assertTrue(stateChanged);
         assertFalse(httpTask.isStarted());
         assertTrue(http2Task.isStarted());
