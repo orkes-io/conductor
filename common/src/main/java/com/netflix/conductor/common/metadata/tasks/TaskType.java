4cb66c22074a9910c1c43d759c8994ef2343475d
manan164 <1897158+manan164@users.noreply.github.com>
2023-08-22 13:47:09 +0530

    Update secret task type

--- common/src/main/java/com/netflix/conductor/common/metadata/tasks/TaskType.java
+++ common/src/main/java/com/netflix/conductor/common/metadata/tasks/TaskType.java
@@ -40,6 +40,7 @@ public enum TaskType {
     TERMINATE,
     KAFKA_PUBLISH,
     JSON_JQ_TRANSFORM,
+    UPDATE_SECRET,
     SET_VARIABLE;
 
     /**
@@ -68,6 +69,8 @@ public enum TaskType {
     public static final String TASK_TYPE_KAFKA_PUBLISH = "KAFKA_PUBLISH";
     public static final String TASK_TYPE_JSON_JQ_TRANSFORM = "JSON_JQ_TRANSFORM";
     public static final String TASK_TYPE_SET_VARIABLE = "SET_VARIABLE";
+    public static final String TASK_TYPE_UPDATE_SECRET = "UPDATE_SECRET";
+
     public static final String TASK_TYPE_FORK = "FORK";
 
     private static final Set<String> BUILT_IN_TASKS = new HashSet<>();
